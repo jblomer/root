94b79553cbf0f77cb6f7dd651d189c737731b3af
Jakob Blomer <jblomer@cern.ch>
2018-07-19 18:08:46 +0200

    [Forest] build system skeleton

---
+++ tree/forest/v7/inc/ROOT/RColumn.hxx
@@ -0,0 +1,24 @@
+/// \file ROOT/RHist.h
+/// \ingroup Forest ROOT7
+/// \author Jakob Blomer <jblomer@cern.ch>
+/// \date 2018-07-19
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2015, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT7_RColumn
+#define ROOT7_RColumn
+
+namespace ROOT {
+namespace Experimental {
+} // namespace Experimental
+} // namespace ROOT
+
+#endif

9711ef56bad491d6c4f657d8b9ffb774b0f6b6d4
Jakob Blomer <jblomer@cern.ch>
2018-07-19 18:08:46 +0200

    [Forest] build system skeleton

---
+++ tree/forest/LinkDef.h
@@ -0,0 +1,16 @@
+// Author: Jakob Blomer CERN  7/2018
+
+/*************************************************************************
+ * Copyright (C) 1995-2018, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                    *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __CLING__
+
+#pragma link C++ nestedtypedefs;
+#pragma link C++ nestedclasses;
+
+#endif

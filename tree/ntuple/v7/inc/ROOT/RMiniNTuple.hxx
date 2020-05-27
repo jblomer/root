4d20da083210c8cde5009385964f3b142bf8ad90
Jakob Blomer <jblomer@cern.ch>
2020-05-27 08:46:13 +0200

    [ntuple] C API minor improvements

--- tree/ntuple/v7/inc/ROOT/RMiniNTuple.hxx
+++ tree/ntuple/v7/inc/ROOT/RMiniNTuple.hxx
@@ -30,6 +30,10 @@ struct ROOT_ntpl_field {
   struct ROOT_ntpl_field *next;
 };
 
+#define ROOT_NTPL_COLLECTION  0
+#define ROOT_NTPL_FLOAT       1
+#define ROOT_NTPL_DOUBLE      2
+
 ROOT_ntpl *ROOT_ntpl_open(const char *ntpl_name, const char *path);
 void ROOT_ntpl_close(ROOT_ntpl *ntpl);
 
@@ -39,11 +43,12 @@ void ROOT_ntpl_list_free(ROOT_ntpl_field *list);
 
 // Iteration
 bool ROOT_ntpl_entry_next(ROOT_ntpl *ntpl);
-void ROOT_ntpl_entry_reset(ROOT_ntpl *ntpl);
+void ROOT_ntpl_entry_rewind(ROOT_ntpl *ntpl);
 bool ROOT_ntpl_collection_next(ROOT_ntpl_view *view);
-void ROOT_ntpl_collection_reset(ROOT_ntpl_view *view);
+void ROOT_ntpl_collection_rewind(ROOT_ntpl_view *view);
 
-ROOT_ntpl_view *ROOT_ntpl_view(void *ntpl_or_view, const char *field);
+// Runtime type check
+ROOT_ntpl_view *ROOT_ntpl_view(void *ntpl_or_view, const char *field, int type);
 
 float ROOT_ntpl_float(ROOT_ntpl_view *view);
 double ROOT_ntpl_double(ROOT_ntpl_view *view);
@@ -55,9 +60,10 @@ int ROOT_ntpl_size(void *ntpl_or_view);
 
 ROOT_ntpl *ntpl = ROOT_ntpl_open("MyNtuple", "data.root");
 
-ROOT_ntpl_view *pt           = ROOT_ntpl_view(ntpl,   "pt");
-ROOT_ntpl_view *tracks       = ROOT_ntpl_view(ntpl,   "tracks");
-ROOT_ntpl_view *track_energy = ROOT_ntpl_view(tracks, "E"); // accesses tracks.E
+ROOT_ntpl_view *pt           = ROOT_ntpl_view(ntpl,   "pt",     ROOT_NTPL_FLOAT);
+ROOT_ntpl_view *tracks       = ROOT_ntpl_view(ntpl,   "tracks", ROOT_NTPL_COLLECTION);
+// accesses tracks.E
+ROOT_ntpl_view *track_energy = ROOT_ntpl_view(tracks, "E",      ROOT_NTPL_FLOAT);
 
 printf("%d entries\n"), ROOT_ntpl_size(ntpl));
 

4a5dba7c931412ef5ab9922c7404814150e4bcb3
Jakob Blomer <jblomer@cern.ch>
2020-09-07 23:52:09 +0200

    [ntuple] Provide interface class for the page storage task scheduler

--- tree/ntuple/v7/inc/ROOT/RPageStorageFile.hxx
+++ tree/ntuple/v7/inc/ROOT/RPageStorageFile.hxx
@@ -155,7 +155,7 @@ private:
 
 protected:
    RNTupleDescriptor AttachImpl() final;
-   void UnzipClusterImpl(RCluster *cluster, TaskScheduleFunc_t taskScheduleFunc) final;
+   void UnzipClusterImpl(RCluster *cluster) final;
 
 public:
    RPageSourceFile(std::string_view ntupleName, std::string_view path, const RNTupleReadOptions &options);

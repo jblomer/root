9c1a3ec1c1ff4de7f511aac5aa8a9f07acbc2eb0
Jakob Blomer <jblomer@cern.ch>
2024-08-21 00:14:49 +0200

    [ntuple] make RColumnHandle::fColumn non-const

--- tree/ntuple/v7/test/ntuple_storage.cxx
+++ tree/ntuple/v7/test/ntuple_storage.cxx
@@ -25,7 +25,7 @@ public:
    } fCounters{};
 
 protected:
-   ColumnHandle_t AddColumn(ROOT::Experimental::DescriptorId_t, const ROOT::Experimental::Internal::RColumn &) final
+   ColumnHandle_t AddColumn(ROOT::Experimental::DescriptorId_t, ROOT::Experimental::Internal::RColumn &) final
    {
       return {};
    }

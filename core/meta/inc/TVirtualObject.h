cd955fec6e3fbd49387cb81e36787ed91f4a02ce
Jakob Blomer <jblomer@cern.ch>
2025-10-06 17:34:47 +0200

    style update of TVirtualObject

--- core/meta/inc/TVirtualObject.h
+++ core/meta/inc/TVirtualObject.h
@@ -12,34 +12,34 @@
 #ifndef ROOT_TVirtualObject
 #define ROOT_TVirtualObject
 
-/**
-\class TVirtualObject
-\ingroup IO
-
-Wrapper around an object and giving indirect access to its content
-even if the object is not of a class in the Cint/Reflex dictionary.
-*/
-
 #include "TClassRef.h"
 
-
+/// \class TVirtualObject
+/// \ingroup IO
+///
+/// Wrapper around an object and giving indirect access to its content
+/// even if the object is not of a class in the Cint/Reflex dictionary.
+///
+/// The TVirtualObject represents the schema evolution class / staging area,
+/// i.e. the description of the conversion between different class versions.
 class TVirtualObject {
-private:
-
-   TVirtualObject(const TVirtualObject&) = delete;
-   TVirtualObject &operator=(const TVirtualObject&) = delete;
-
 public:
-   TClassRef  fClass;
-   void      *fObject;
+   TClassRef  fClass;  ///< Corresponds to the conversion streamer info ("Class@@Version")
+   void      *fObject; ///< The staging area of the on-disk class members required during I/O rule execution
 
-   TVirtualObject(TClass *cl) : fClass(cl), fObject(cl ? cl->New() : nullptr) { }
-   ~TVirtualObject() { if (fClass) fClass->Destructor(fObject); }
-
+   TVirtualObject(TClass *cl) : fClass(cl), fObject(cl ? cl->New() : nullptr) {}
+   TVirtualObject(const TVirtualObject &) = delete;
+   TVirtualObject(TVirtualObject &&) = delete;
+   TVirtualObject &operator=(const TVirtualObject &) = delete;
+   TVirtualObject &operator=(TVirtualObject &&) = delete;
+   ~TVirtualObject()
+   {
+      if (fClass)
+         fClass->Destructor(fObject);
+   }
 
    TClass *GetClass() const { return fClass; }
    void   *GetObject() const { return fObject; }
-
 };
 
 #endif // ROOT_TVirtualObject
